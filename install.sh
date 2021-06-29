c8afd7e8baf1262a509a22c6e07a7c25f0242104
L0G1K200 <75788512+L0G1K200@users.noreply.github.com>
2021-06-29 14:04:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN="\e[32m"
 apt install figlet -y
 clear
 figlet "StrongDDos"
-echo "\n"
-echo "${RED}[*] Started installing StrongDDos script!"
-echo "${NC}\n"
+echo " "
+echo "[*] Started installing StrongDDos script!"
+echo " "
 apt install python2 python3
 clear
 figlet "All Packages Successfully installed"
-echo "${GREEN}[*] Now you can run script with command 'python2 script.py'"
+echo "[*] Now you can run script with command 'python2 script.py'"
